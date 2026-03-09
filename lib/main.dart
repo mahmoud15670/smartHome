cfe1aea93fce6ccd09f21152ef6c3a9eacd45d8f
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2026-03-09 15:45:09 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:smarthome/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print("app ............");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AuthWrapper(),
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        }
+        return LoginPage();
+      },
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: email,
+              decoration: InputDecoration(hintText: "Email"),
+            ),
+            TextField(
+              controller: password,
+              decoration: InputDecoration(hintText: "Password"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email.text,
+                  password: password.text,
+                );
+              },
+              child: Text("Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool relayState = false;
+  late DatabaseReference ref;
+
+  @override
+  void initState() {
+    super.initState();
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    ref = FirebaseDatabase.instance.ref("devices/$uid/relay");
+
+    ref.onValue.listen((event) {
+      if (event.snapshot.value != null) {
+        setState(() {
+          relayState = event.snapshot.value as bool;
+        });
+      }
+    });
+  }
+
+  void toggle() {
+    ref.set(!relayState);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Smart Home"),
+        actions: [
+          IconButton(
+            onPressed: () => FirebaseAuth.instance.signOut(),
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Switch(value: relayState, onChanged: (val) => toggle()),
+      ),
+    );
+  }
+}
